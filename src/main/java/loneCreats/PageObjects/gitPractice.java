458d3e62e749ae2184577489d4bb022d56074707
Lone-Hassan <lone.hassan@gmail.com>
2024-04-30 22:33:58 +0500

    commit from workspace2

---
+++ src/main/java/loneCreats/PageObjects/gitPractice.java
@@ -0,0 +1,12 @@
+package loneCreats.PageObjects;
+
+public class gitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("project clooned succesfuly.... hurray!!! ");
+
+	}
+
+}
